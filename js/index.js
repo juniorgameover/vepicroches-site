8be50ea27afa61429cc4d7f20636d01c26c05990
juniorgameover <juniorra@gmail.com>
2022-04-15 21:26:13 -0300

    adição de estilos de responsividade

--- js/index.js
+++ js/index.js
@@ -1,9 +1,21 @@
 const botaoMenu = document.querySelector('.expande-menu');
 const menuNavegacao = document.querySelector('.menu');
 
+
 console.log(botaoMenu);
 console.log(menuNavegacao);
 
 botaoMenu.addEventListener('click' , () => {
     menuNavegacao.classList.toggle('menu-ativo');
 })
+
+window.addEventListener('resize' , () => {
+    let larguraDaTela = window.innerWidth;
+    console.log(larguraDaTela);
+    if(larguraDaTela > 768){
+       menuNavegacao.classList.remove('menu-ativo');
+    }
+})
+
+
+
